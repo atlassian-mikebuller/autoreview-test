37406a5dfb73f2334b7a60560943dfc4f11ece29
atlassian-mikebuller <mbuller@atlassian.com>
2025-02-21 15:23:29 +1100

    Create cs-test.kt

---
+++ cs-test.kt
@@ -0,0 +1,14 @@
+class CrapCode {
+    val largeList = List(1000000) { it }
+
+    // Inefficient way to calculate the sum of elements
+    var sum = 0
+    fun calculateSum() {
+        for (i in 0 until largeList.size) {
+            sum += largeList[i]
+        }
+    }
+
+    // typo
+    prentln("Spelling error")
+}
