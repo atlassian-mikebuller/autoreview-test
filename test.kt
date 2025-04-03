5d02354a62c9e0176bf158a653a760dae06474d7
Mike Buller <mbuller@atlassian.com>
2025-04-03 21:48:50 +1100

    Code

---
+++ test.kt
@@ -0,0 +1,12 @@
+fun MyKewlFunction() {
+    println("line 1")
+    println("line 2")
+    println("line 3")
+    println("line 4")
+    println("line 5")
+    println("line 6")
+    println("line 7")
+    println("line 8")
+    println("line 9")
+    println("line 10")
+}
